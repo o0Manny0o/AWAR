e93bf9cbc96b88e3d54acf2182d3fc444866ae59
o0Manny0o <moritz.wach@t-online.de>
2024-10-24 18:46:26 +0200

    Add trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->trustProxies();
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
