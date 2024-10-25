f490b003fb88782015745b254bddce5a0f9644d2
o0Manny0o <moritz.wach@t-online.de>
2024-10-25 14:27:44 +0200

    Implement multi tenancy basics

---
+++ app/Models/Organisation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\Database\Models\Tenant;
+
+class Organisation extends Tenant implements TenantWithDatabase
+{
+
+    use HasDatabase, HasDomains;
+
+    protected $table = 'organisations';
+}
