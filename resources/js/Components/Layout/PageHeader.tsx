ba3668afb90ca50f3f5c1efde12f638968d86885
o0Manny0o <moritz.wach@t-online.de>
2024-11-24 14:57:44 +0100

    Add back button (WIP) to page header

--- resources/js/Components/Layout/PageHeader.tsx
+++ resources/js/Components/Layout/PageHeader.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonColorVariants } from '@/Components/_Base/Button'
 import { Method } from '@inertiajs/core'
 import { Badge, BadgeColor } from '@/Components/_Base/Badge'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 
 export type PageHeaderButton = {
     label: string
@@ -16,16 +17,24 @@ interface PageHeaderProps {
         label: string
     }
     actionButtons?: PageHeaderButton[]
+    backUrl?: string
 }
 
 export default function PageHeader({
     title,
     badge,
     actionButtons,
+    backUrl,
 }: PageHeaderProps) {
     return (
         <div className="flex items-baseline justify-between">
             <div className="flex items-center gap-2">
+                {/* TODO: Improve style */}
+                {backUrl && (
+                    <Button color="secondary" href={backUrl} className="me-4">
+                        <ChevronLeftIcon className="size-5" />
+                    </Button>
+                )}
                 <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
                     {title}
                 </h2>
